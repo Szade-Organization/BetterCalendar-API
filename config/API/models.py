bb979ca654173baf2381655b40099147432a90fb
Szade <wojciech.szade@gmail.com>
2023-12-18 12:57:43 +0100

    Changed models and added better default values and simple validation

--- config/API/models.py
+++ config/API/models.py
@@ -1,5 +1,6 @@
 from djongo import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
 
 class User(AbstractUser):
@@ -12,38 +13,42 @@ class Category(models.Model):
         ('S', 'Should'),
         ('C', 'Could'),
         ('W', 'Would'),
-        ('N', 'None')
+        ('N', 'None'),
+        ('n', 'DefaultNone')
     ]
     name = models.CharField(max_length=128)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    # TODO: przetestować czy to w dobrą stronę usuwanie
     description = models.CharField(max_length=256, blank=True)
-    # TODO: jak to przechowujemy?
-    color = models.CharField(max_length=128, blank=True)
-    # TODO: jak to przechowujemy?
+    color = models.CharField(max_length=10, blank=True, help_text="hex")
     icon = models.CharField(max_length=128, blank=True)
     importance_level = models.CharField(
-        max_length=1, choices=IMPORTANCE_LEVEL_CHOICES, default='N')
+        max_length=1, choices=IMPORTANCE_LEVEL_CHOICES, default='n')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
 
 class Activity(models.Model):
     name = models.CharField(max_length=128)
-    # TODO: przetestować czy to w dobrą stronę usuwanie
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    # TODO: przetestować czy to w dobrą stronę usuwanie
     category = models.ForeignKey(
         to=Category, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=256, blank=True)
     date_start = models.DateTimeField(null=True)
     date_end = models.DateTimeField(null=True)
-    # TODO: add default to date_end - date_start in clean or save
     length = models.DurationField(null=True)
-    # TODO: if date_start and date_end is not null then is_planned = True
     is_planned = models.BooleanField(default=False)
-    # TODO: add default value to self.category.importance_level to clean or save
     importance_level = models.CharField(
-        max_length=1, choices=Category.IMPORTANCE_LEVEL_CHOICES, default='N')
+        max_length=1, choices=Category.IMPORTANCE_LEVEL_CHOICES, default='n')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.date_start and self.date_end:
+            if self.date_start > self.date_end:
+                raise ValidationError(
+                    'date_start must be less than or equal to date_end')
+            self.length = self.date_end - self.date_start
+            self.is_planned = True
+        if self.importance_level == 'n':
+            self.importance_level = self.category.importance_level
+        super(Activity, self).save(*args, **kwargs)
