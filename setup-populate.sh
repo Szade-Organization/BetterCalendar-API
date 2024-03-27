251691b182c20f44963ad59b03e546573810f865
Wojciech Szade <wojciech.szade@gmail.com>
2024-03-27 14:31:07 +0100

    Updated setup-populate.sh

--- setup-populate.sh
+++ setup-populate.sh
@@ -8,13 +8,14 @@ if [ -n "$POPULATE_INITIALIZED" ]; then
     echo "Populate already initiated - skipping"
     return 0
 fi
-if python config/manage.py migrate; then
+cd config
+if python manage.py migrate; then
     echo "Migrations applied"
 else
     echo "Migrations failed"
     return 1
 fi
-if python config/manage.py populate -u 3 -c 40 -a 400; then
+if python manage.py populate -u 3 -c 40 -a 400; then
     echo "Database populated"
 else
     echo "Database population failed"
